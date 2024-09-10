d133beccedbe2e8ce7fe571ce75a1487f672b2d7
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-10 15:49:23 +0200

    Define class that validates personnummer

---
+++ src/Conditionals6.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Conditionals6 {
+    public void run() {
+        String personnr = providePersonnummer();
+        isPersonnummerValid(personnr);
+
+
+    }
+
+    /**
+     * Ask user to provide a 10-digit personnummer.
+     * @return personnummer.
+     */
+    private String providePersonnummer() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter personnummer, 10 digits: ");
+        return input.nextLine();
+    }
+
+    /**
+     * Validates personnummer.
+     * @param personnummer - User provided personnummer, 10-digits.
+     */
+    private void isPersonnummerValid(String personnummer) {
+        int controlDigit = Character.getNumericValue(personnummer.charAt(personnummer.length() - 1));
+        boolean isMultipleOfTwo = true;
+        String addNumbersToString = "";
+        int sum = 0;
+
+        for (int i = 0; i < personnummer.length() - 1; i++) {
+            int digit = Character.getNumericValue(personnummer.charAt(i));
+            if (isMultipleOfTwo) {
+                addNumbersToString += digit * 2;
+            } else {
+                addNumbersToString += digit;
+            }
+            isMultipleOfTwo = !isMultipleOfTwo;
+        }
+
+        for (int i = 0; i < addNumbersToString.length(); i++) {
+            sum += Integer.parseInt(addNumbersToString.charAt(i) + "");
+        }
+
+        if ((10 - (sum % 10)) % 10 == controlDigit) {
+            System.out.println("Valid personnummer");
+        } else {
+            System.out.println("Invalid personnummer");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Conditionals5 placeholder = new Conditionals5();
+        Conditionals6 placeholder = new Conditionals6();
         placeholder.run();
     }
 }
