97a36aa18fb1d3ef63e61179972dc82af7a03d23
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-10 14:48:51 +0200

    Define class for fizzbuzz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Conditionals3 placeholder = new Conditionals3();
+        Conditionals4 placeholder = new Conditionals4();
         placeholder.run();
     }
 }
